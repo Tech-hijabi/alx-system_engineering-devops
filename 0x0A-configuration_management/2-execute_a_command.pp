05dc6da7c9ed31dad15c1a779a343d8709105dcf
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2024-02-28 16:05:42 -0800

    fixing all tasks

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Kills process killmenow
 
 exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
